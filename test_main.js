e101934b954c79d66e88a1b533e20c430a1a73e3
rolson24 <raif.t.olson.24@dartmouth.edu>
2024-03-10 18:28:47 -0400

    hide reprocess button

--- test_main.js
+++ test_main.js
@@ -317,13 +317,13 @@ app.on('ready', () => {
     startFlaskApp();
     createWindow();
     // Simulate the computer going to sleep after 3 minutes
-    setTimeout(() => {
-        powerMonitor.emit('suspend');
-        // Simulate the computer waking up after 3 more minutes
-        setTimeout(() => {
-            powerMonitor.emit('resume');
-        }, 120000); // 180000 milliseconds = 3 minutes
-    }, 120000); // 180000 milliseconds = 3 minutes
+    // setTimeout(() => {
+    //     powerMonitor.emit('suspend');
+    //     // Simulate the computer waking up after 3 more minutes
+    //     setTimeout(() => {
+    //         powerMonitor.emit('resume');
+    //     }, 120000); // 180000 milliseconds = 3 minutes
+    // }, 120000); // 180000 milliseconds = 3 minutes
     powerMonitor.on('suspend', () => {
         console.log('The system is going to sleep');
     });
