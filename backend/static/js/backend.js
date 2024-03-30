c5ee9fac5034c111b4bd7e6c62fc445ec377a2ae
Raif Olson <raif.t.olson.24@dartmouth.edu>
2024-03-30 14:00:29 -0400

    clean up object rendering

--- backend/static/js/backend.js
+++ backend/static/js/backend.js
@@ -23,64 +23,6 @@ function handleSocketReconnection(socket) {
 }
 
 
-function drawArrow(ctx, fromx, fromy, tox, toy, arrowWidth, color) {
-    var headlen = 10; // length of head in pixels
-    var dx = tox - fromx;
-    var dy = toy - fromy;
-    var angle = Math.atan2(dy, dx);
-    ctx.strokeStyle = color;
-    ctx.fillStyle = color;
-    ctx.lineWidth = arrowWidth;
-    ctx.beginPath();
-    ctx.moveTo(fromx, fromy);
-    ctx.lineTo(tox, toy);
-    ctx.lineTo(tox - headlen * Math.cos(angle - Math.PI / 6), toy - headlen * Math.sin(angle - Math.PI / 6));
-    ctx.moveTo(tox, toy);
-    ctx.lineTo(tox - headlen * Math.cos(angle + Math.PI / 6), toy - headlen * Math.sin(angle + Math.PI / 6));
-    ctx.stroke();
-}
-
-
-// Function to render the counts data into the table
-function renderCountsTable(countsData) {
-    var tableBody = document.getElementById('counts-display').querySelector('tbody');
-    tableBody.innerHTML = ''; // Clear the table body
-    if (countsData) {
-        countsData.forEach(function(count) {
-            var row = document.createElement('tr');
-            var lineCell = document.createElement('td');
-            var classCell = document.createElement('td');
-            var directionCell = document.createElement('td');
-            var countCell = document.createElement('td');
-
-            lineCell.textContent = count.line;
-            classCell.textContent = count.class;
-            directionCell.textContent = count.direction;
-            countCell.textContent = count.count;
-
-            row.appendChild(lineCell);
-            row.appendChild(classCell);
-            row.appendChild(directionCell);
-            row.appendChild(countCell);
-            tableBody.appendChild(row);
-        });
-    }
-}
-
-// Function to render the plot using Plotly
-function renderPlot(plotData, plotElementId, plotName) {
-    if (plotData) {
-        Plotly.newPlot(plotElementId, plotData.data, plotData.layout, {
-            toImageButtonOptions: {
-                filename: plotName,
-                height: null,
-                width: null,
-                format: 'png'
-            }
-        });
-    }
-}
-
 var processing = false;
 var labels = {};  // Create an object to store the labels
 
@@ -106,7 +48,7 @@ $(document).ready(function () {
     // Load state and plot data from local storage
     loadStateFromLocalStorage(document);
     var countsData = loadCountsDataFromLocalStorage();
-    renderCountsTable(countsData);
+    renderCountsTable(countsData, document);
     var countsPlotData = loadPlotDataFromLocalStorage('countsPlotData');
     renderPlot(countsPlotData, 'counts-plot', 'counts_by_line');
     var crossingsPlotData = loadPlotDataFromLocalStorage('crossingsPlotData');
